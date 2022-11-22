6ba9c06a7144f9e9fb62a94cd8731b1e30e193e5
ChoiDevv <oinkoink71@naver.com>
2022-11-22 14:29:16 +0900

    add: static

---
+++ Foundation/src/Static/Whosegoods.java
@@ -0,0 +1,23 @@
+package Static;
+
+public class Whosegoods {
+
+    String name = "whosegoods";
+    static String staticName = "whosegoods";
+    int people = 0;
+    static int staticPeople = 0;
+
+    public Whosegoods() {
+        people++;
+        System.out.println("Primitive");
+        System.out.println(people);
+
+        System.out.println("Static");
+        staticPeople++;
+        System.out.println(staticPeople);
+    }
+
+    public static int getStaticPeople() {
+        return staticPeople;
+    }
+}
