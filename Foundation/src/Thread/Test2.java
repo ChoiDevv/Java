0b7cea1e9c39c5955570e7ba7885983cb4281a1a
ChoiDevv <oinkoink71@naver.com>
2022-11-25 16:55:38 +0900

    add: Thread

---
+++ Foundation/src/Thread/Test2.java
@@ -0,0 +1,41 @@
+package Thread;
+
+import java.util.ArrayList;
+
+public class Test2 extends Thread {
+
+    int seq;
+
+    public Test2(int seq) {
+        this.seq = seq;
+    }
+
+    public void run() {
+        System.out.println(this.seq + " Thread Start");
+        try {
+            Thread.sleep(1000);
+        } catch (Exception e) {
+
+        }
+        System.out.println(this.seq + " Thread End");
+    }
+
+    public static void main(String[] args) {
+        ArrayList<Thread> threads = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            Thread t = new Test(i);
+            t.start();
+            threads.add(t);
+        }
+
+        for (int i = 0; i < 10; i++) {
+            Thread t = threads.get(i);
+            try {
+                t.join();
+            } catch (Exception e) {
+
+            }
+        }
+        System.out.println("main end");
+    }
+}
---
+++ Foundation/src/Thread/Test3.java
@@ -0,0 +1,43 @@
+package Thread;
+
+import java.util.ArrayList;
+
+public class Test3 implements Runnable {
+
+    int seq;
+
+    public Test3(int seq) {
+        this.seq = seq;
+    }
+
+    @Override
+    public void run() {
+        System.out.println(this.seq + " Thread Start");
+        try {
+            Thread.sleep(1000);
+        } catch (Exception e) {
+
+        }
+        System.out.println(this.seq + " Thread End");
+    }
+
+    public static void main(String[] args) {
+        ArrayList<Thread> threads = new ArrayList<>();
+
+        for (int i = 0; i < 10; i++) {
+            Thread t = new Thread(new Test3(i));
+            t.start();
+            threads.add(t);
+        }
+
+        for (int i = 0; i < 10; i++) {
+            Thread t = threads.get(i);
+            try {
+                t.join();
+            } catch (Exception e) {
+
+            }
+        }
+        System.out.println("main End");
+    }
+}
