0ace3b4485e9db7cb2f44b1462f82ee43f62ee62
ChoiDevv <oinkoink71@naver.com>
2022-11-29 17:00:02 +0900

    add: Stack

--- Foundation/src/DataStructure/Stack/StackExample.java
+++ Foundation/src/DataStructure/Stack/StackExample.java
@@ -1,4 +1,20 @@
 package DataStructure.Stack;
 
+import java.util.Stack;
+
 public class StackExample {
+    public static void main(String[] args) {
+        Stack<String> stack = new Stack<>();
+
+        stack.push("Hi");
+        stack.push("I'm");
+        stack.push("Choi Seongdae");
+
+        System.out.println(stack);
+
+        stack.pop();
+        System.out.println(stack);
+
+        System.out.println(stack.peek());
+    }
 }
